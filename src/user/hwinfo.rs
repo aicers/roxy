9ec26fd757109fa0c0064d03219e382409475bf7
Sehkone Kim <sehkone@petabi.com>
2022-11-11 13:17:03 +0900

    Add `resource_usage` and `hostname`

--- src/user/hwinfo.rs
+++ src/user/hwinfo.rs
@@ -1,53 +1,11 @@
 use super::run_command_output;
-use anyhow::Result;
-use regex::Regex;
 use std::{fmt::Write as FmtWrite, fs::File, io::Read};
 
-const DATA_PARTITION: &str = "/data";
 const DEFAULT_VERSION_STRING: &str = "AICE security";
 // TODO: should change this path to /usr/local/aice/conf/version?
 const DEFAULT_VERSION_PATH: &str = "/etc/version";
 
-// Get the usage of the partition mounted on `/data` using command `df -h`:
-//
-// # Return
-//
-//   Mount point, Total size, Used size, Used rate
-//
-// # Errors
-//
-// * fail to compile regex
-pub(crate) fn disk_usage() -> Result<Option<(String, String, String, String)>> {
-    if let Some(output) = run_command_output("df", None, &["-h"]) {
-        let re = Regex::new(
-            r#"(?P<f>[/a-z0-9]+)\s+(?P<s>[0-9\.]+[A-Za-z]+)\s+(?P<u>[0-9\.]+[A-Za-z]*)\s+(?P<a>[0-9\.]+[A-Za-z]*)\s+(?P<e>[0-9]+%)\s+(?P<m>[/a-z0-9]+)"#,
-        )?;
-        let lines = output.lines();
-        for line in lines {
-            if line.starts_with("/dev/") && line.ends_with(DATA_PARTITION) {
-                let after = re.replace_all(line, "$m,$s,$u,$e");
-                let mut values = after.as_ref().split(',');
-                if let Some(mount) = values.next() {
-                    if let Some(size) = values.next() {
-                        if let Some(used) = values.next() {
-                            if let Some(rate) = values.next() {
-                                return Ok(Some((
-                                    (*mount).to_string(),
-                                    (*size).to_string(),
-                                    (*used).to_string(),
-                                    (*rate).to_string(),
-                                )));
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    Ok(None)
-}
-
-/// Get system uptime. It's joined result for command `uptime -p` and `uptime -s`
+/// Returns how long the system has been running by `uptime -p` and `uptime -s`
 ///
 /// # Example
 ///
@@ -55,11 +13,12 @@ pub(crate) fn disk_usage() -> Result<Option<(String, String, String, String)>> {
 /// // Result format:
 /// // how long the system has been running (boot: boot up time)
 /// // up 7 weeks, 5 days, 13 hours, 52 minutes (boot: 2021-12-16 23:43:10)
-/// if let Some(uptime) = hwinfo::uptime() {
+/// if let Some(uptime) = uptime() {
 ///     println!("uptime = {}", uptime);
 /// }
 /// ```
-pub(crate) fn uptime() -> Option<String> {
+#[must_use]
+pub fn uptime() -> Option<String> {
     let mut status = String::new();
     if let Some(mut output) = run_command_output("uptime", None, &["-p"]) {
         output.pop();
@@ -77,15 +36,16 @@ pub(crate) fn uptime() -> Option<String> {
     }
 }
 
-/// Get OS and Product versions. Refer /etc/version
+/// Returns OS and Product versions by reading /etc/version.
 ///
 /// # Example
 ///
 /// ```ignore
-/// let (os_ver, product_ver) = hwinfo::get_version();
+/// let (os_ver, product_ver) = version();
 /// println!("OS version = {}, Product version = {}", os_ver, product_ver);
 /// ```
-pub(crate) fn get_version() -> (String, String) {
+#[must_use]
+pub fn version() -> (String, String) {
     let mut os_version = DEFAULT_VERSION_STRING.to_string();
     let mut product_version = DEFAULT_VERSION_STRING.to_string();
     if let Ok(mut file) = File::open(DEFAULT_VERSION_PATH) {
